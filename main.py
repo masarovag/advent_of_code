50f5ee287cbf9aead0969a7142a94cf56a652120
LAPTOP-EPT4T92E\masar <masarova.g@gmail.com>
2022-12-11 20:12:48 +0100

    Rock, paper, scissors task

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from tasks_2021.task_1 import sonar
 from tasks_2022.task_1 import elves_calories
 from tasks_2022.task_2 import count_score
 
-count_score()
+
+
+
+
--- tasks_2022/task_2.py
+++ tasks_2022/task_2.py
@@ -4,5 +4,35 @@ def count_score():
     file_to_open = data_folder / "data_2.txt"
     f = open(file_to_open)
     lines = f.readlines()
+    score = 0
 
-    print(lines)
+    for i in lines:
+        values = to_int_list(i)
+        score += calculate_line_score(values[0], values[1])
+
+    return score
+
+
+def calculate_line_score(opponent, me):
+    if opponent == me:
+        return 3 + me
+    elif (opponent == 1 and me == 2) or (opponent == 2 and me == 3) or (opponent == 3 and me == 1):
+        return 6 + me
+    else:
+        return me
+
+
+def convert_string_to_int(character):
+    if character == "A" or character == "X":
+        return 1
+    elif character == "B" or character == "Y":
+        return 2
+    else:
+        return 3
+
+
+def to_int_list(value):
+    string_list = value.replace("\n", "").split(" ")
+    opponent = convert_string_to_int(string_list[0])
+    me = convert_string_to_int(string_list[1])
+    return [opponent, me]
